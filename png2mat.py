23b1592ab3ce2cd73a9bb97245c02f5db8edd78d
shen65536 <shen65536@outlook.com>
2021-05-10 21:41:19 +0800

    Update png2mat.py

--- png2mat.py
+++ png2mat.py
@@ -13,6 +13,6 @@ print('Start...')
 for i in range(file_list.__len__()):
     img = Image.open(file_list[i])
     res = image_transforms(img)
-    res = np.array(res, dtype=np.float32) / 255
+    res = np.array(res, dtype=np.float32) / 255.0
     io.savemat(save_dir + '/({}).mat'.format(i + 1), {'temp3': res})
 print('done')

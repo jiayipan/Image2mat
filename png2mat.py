da80dd534bc1d63b1c187ef8c55dfc9703682b88
shen65536 <shen65536@outlook.com>
2021-05-11 19:35:18 +0800

    Update png2mat.py

--- png2mat.py
+++ png2mat.py
@@ -1,18 +1,19 @@
 import glob
 import numpy as np
-from PIL import Image
 import scipy.io as io
-import torchvision.transforms as transforms
+import torchvision
+from PIL import Image
 
 src_dir = '/Users/xxx/Documents/数据集/URBAN100_PNG/'
 save_dir = '/Users/xxx/Documents/数据集/URBAN100_MAT'
 file_list = glob.glob(src_dir+'*.png')
-image_transforms = transforms.Compose([transforms.ToTensor(), transforms.Grayscale(1)])
 print('Start...')
 
 for i in range(file_list.__len__()):
     img = Image.open(file_list[i])
-    res = image_transforms(img)
-    res = np.array(res, dtype=np.float32) / 255.0
-    io.savemat(save_dir + '/({}).mat'.format(i + 1), {'temp3': res})
+
+    img = torchvision.transforms.ToTensor()(img)
+    img = torchvision.transforms.Grayscale(num_output_channels=1)(img).squeeze()
+    img = img.numpy()
+    io.savemat(save_dir + '/({}).mat'.format(i + 1), {'temp3': img})
 print('done')
